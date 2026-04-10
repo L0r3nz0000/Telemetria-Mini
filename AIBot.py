b8ed9fd58baeb08f860d1d40ee7e50fbb938b55b
Lorenzo <lorenzobefera0@gmail.com>
2026-04-10 10:49:05 +0200

    Test button

--- AIBot.py
+++ AIBot.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
   if GPIO.input(16) == 0:
